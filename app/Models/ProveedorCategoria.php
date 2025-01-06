5dd358d71b7c768ae9d7806b3b69ad4da55c9cda
rguevarac85 <rguevarac85@hotmail.com>
2025-01-06 16:49:50 -0500

    Archivos generados por f¿defecto por la aplicacion que serviran de modelo para poder ver que campos se modifican para que funcioinen correctamente despues reailizados los cambios

---
+++ app/Models/ProveedorCategoria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+use App\Http\Requests\ProveedorCategoria\StoreRequest;
+        use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProveedorCategoria extends Model
+{
+    use HasFactory;    
+
+    protected $fillable = ['nombre', 'estado_activo'];
+}
