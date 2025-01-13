53c1bca57637d1dec37c03354e03260133f24892
rguevarac85 <rguevarac85@hotmail.com>
2025-01-13 04:12:39 -0500

    Se limpia el archivo de rutas y se crea el controlador Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,31 @@
 <?php
 
 use App\Http\Controllers\CategoriaProveedorController;
-use App\Http\Controllers\ClaseServicioController;
 use App\Http\Controllers\CostoController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DestinoController;
 use App\Http\Controllers\DistribucionVentaController;
 use App\Http\Controllers\ServicioClaseController;
 use App\Http\Controllers\ServicioDetalleController;
 use App\Http\Controllers\ProveedorCategoriaController;
 use App\Http\Controllers\ProveedorController;
+use App\Http\Controllers\ProveedorServicioController;
+use App\Http\Controllers\ServicioController;
 use App\Http\Controllers\TipoDocumentoController;
-use App\Models\proveedor;
-use App\Models\ServicioClase;
-use App\Models\ServicioDetalle;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// No autenticadas
+Route::get('/', [DashboardController::class, 'index']); 
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    // /Autenticadas
+    Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
     Route::get('/dashboard/categoria_proveedor', [CategoriaProveedorController::class, 'index'])->name('categoria_proveedor');
     Route::get('/dashboard/categoria_proveedor/create', [CategoriaProveedorController::class, 'create'])->name('categoria_proveedor.create');
     Route::post('/dashboard/categoria_proveedor', [CategoriaProveedorController::class, 'store'])->name('categoria_proveedor.store');
     Route::get('/dashboard/categoria_proveedor/{categoria_proveedor}/edit', [CategoriaProveedorController::class, 'edit'])->name('categoria_proveedor.edit');
     Route::patch('/dashboard/categoria_proveedor/{categoria_proveedor}/update', [CategoriaProveedorController::class, 'update'])->name('categoria_proveedor.update');
     Route::delete('/dashboard//categoria_proveedor/{categoria_proveedor}/destroy', [CategoriaProveedorController::class, 'destroy'])->name('categoria_proveedor.destroy');
-
-    Route::get('/dashboard/clase_servicio', [ClaseServicioController::class, 'index'])->name('clase_servicio');
-    Route::get('/dashboard/clase_servicio/create', [ClaseServicioController::class, 'create'])->name('clase_servicio.create');
-    Route::post('/dashboard/clase_servicio', [ClaseServicioController::class, 'store'])->name('clase_servicio.store');
-    Route::get('/dashboard/clase_servicio/{clase_servicio}/edit', [ClaseServicioController::class, 'edit'])->name('clase_servicio.edit');
-    Route::put('/dashboard/clase_servicio/{clase_servicio}', [ClaseServicioController::class, 'update'])->name('clase_servicio.update');
-    Route::patch('/dashboard/clase_servicio/{clase_servicio}/update', [ClaseServicioController::class, 'update'])->name('clase_servicio.update');
-    Route::delete('/dashboard/clase_servicio/{clase_servicio}/destroy', [ClaseServicioController::class, 'destroy'])->name('clase_servicio.destroy');
 
     // Forma simplificada de las rutas 
     // Comando para listar las Rutas, ---->> php artisan r:l
@@ -112,5 +89,19 @@ Route::middleware([
     Route::get('/dashboard/distribucion_venta/{distribucion_venta}/edit', [DistribucionVentaController::class, 'edit'])->name('distribucion_venta.edit');
     Route::patch('/dashboard/distribucion_venta/{distribucion_venta}/update', [DistribucionVentaController::class, 'update'])->name('distribucion_venta.update');
     Route::delete('/dashboard/distribucion_venta/{distribucion_venta}/destroy', [DistribucionVentaController::class, 'destroy'])->name('distribucion_venta.destroy');
+
+    Route::get('/dashboard/servicio', [ServicioController::class, 'index'])->name('servicio');
+    Route::get('/dashboard/servicio/create', [ServicioController::class, 'create'])->name('servicio.create');
+    Route::post('/dashboard/servicio', [ServicioController::class, 'store'])->name('servicio.store');
+    Route::get('/dashboard/servicio/{servicio}/edit', [ServicioController::class, 'edit'])->name('servicio.edit');
+    Route::patch('/dashboard/servicio/{servicio}/update', [ServicioController::class, 'update'])->name('servicio.update');
+    Route::delete('/dashboard/servicio/{servicio}/destroy', [ServicioController::class, 'destroy'])->name('servicio.destroy');
+
+    Route::get('/dashboard/proveedor_servicio', [ProveedorServicioController::class, 'index'])->name('proveedor_servicio');
+    Route::get('/dashboard/proveedor_servicio/create', [ProveedorServicioController::class, 'create'])->name('proveedor_servicio.create');
+    Route::post('/dashboard/proveedor_servicio', [ProveedorServicioController::class, 'store'])->name('proveedor_servicio.store');
+    Route::get('/dashboard/proveedor_servicio/{proveedor_servicio}/edit', [ProveedorServicioController::class, 'edit'])->name('proveedor_servicio.edit');
+    Route::patch('/dashboard/proveedor_servicio/{proveedor_servicio}/update', [ProveedorServicioController::class, 'update'])->name('proveedor_servicio.update');
+    Route::delete('/dashboard/proveedor_servicio/{proveedor_servicio}/destroy', [ProveedorServicioController::class, 'destroy'])->name('proveedor_servicio.destroy');
 
 });
