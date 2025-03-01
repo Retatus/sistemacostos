de42247de4334f892a272b6589fcbe99ff655858
rguevarac85 <rguevarac85@hotmail.com>
2025-03-01 11:54:19 -0500

    Correccion de error de zona horaria de datepicker

--- resources/js/Components/Datepicker.vue
+++ resources/js/Components/Datepicker.vue
@@ -2,9 +2,10 @@
   <div class="block text-sm font-medium text-gray-700">
     <label v-if="label" class="font-bold">{{ label }}</label>
     <VueDatepicker 
-      v-model="fechaInterna" 
+      v-model="selectedDate"
+      :locale="locale"
+      :timezone="timeZone"
       :format="formatearFecha"
-      :locale="es" 
       @update:modelValue="emitirCambio"
       :class="'mt-1 w-full border-gray-300 rounded-md shadow-sm'"
     />
@@ -12,10 +13,14 @@
 </template>
 
 <script setup>
-import { ref, watch, computed } from "vue";
+import { ref, computed } from "vue";
 import VueDatepicker from "vue3-datepicker";
 import { es } from "date-fns/locale";
 import { format } from "date-fns"; // Importamos la función format de date-fns
+
+const selectedDate = ref(new Date());
+const timeZone = 'America/Lima'; // Zona horaria de Lima
+const locale = es; // Idioma español
 
 const props = defineProps({
   modelValue: [String, Date, null],
@@ -28,25 +33,14 @@ const props = defineProps({
 
 const emit = defineEmits(["update:modelValue"]);
 
-const fechaInterna = ref(props.modelValue ? ajustarZonaHoraria(props.modelValue) : null);
-
-function ajustarZonaHoraria(fechaString) {
-    const fecha = new Date(fechaString);
-    fecha.setHours(fecha.getHours() + fecha.getTimezoneOffset() / 60); // Ajuste a la hora local
-    return fecha;
-}
 // Función para formatear la fecha
 const formatearFecha = computed(() => {
-  return fechaInterna.value ? format(fechaInterna.value, props.format) : "";
+  return selectedDate.value ? format(selectedDate.value, props.format) : "";
 });
 
 // Emitir cambios al padre
 const emitirCambio = () => {
-  emit("update:modelValue", fechaInterna.value ? format(fechaInterna.value, "yyyy-MM-dd") : null);
+  emit("update:modelValue", selectedDate.value ? format(selectedDate.value, "yyyy-MM-dd") : null);
 };
 
-// Sincronizar cambios cuando cambia el prop
-watch(() => props.modelValue, (newVal) => {
-  fechaInterna.value = newVal ? new Date(newVal) : null;
-});
 </script>
