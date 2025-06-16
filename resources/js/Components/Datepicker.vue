3003b371aace4bd0164d83eb81727de38241b6de
rguevarac85 <rguevarac85@hotmail.com>
2025-06-16 21:45:55 +0200

    correccion de datepicker se agrega la zona horaria

--- resources/js/Components/Datepicker.vue
+++ resources/js/Components/Datepicker.vue
@@ -19,7 +19,12 @@ import { ref, computed } from "vue";
 import VueDatePicker from "@vuepic/vue-datepicker";
 import '@vuepic/vue-datepicker/dist/main.css';
 import { es } from "date-fns/locale";
-import dayjs from "dayjs";
+import dayjs from "dayjs";  // Usamos dayjs
+import utc from "dayjs/plugin/utc"; // Plugin para UTC
+import timezone from "dayjs/plugin/timezone"; // Plugin para zonas horarias
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 // Props y valores iniciales
 const props = defineProps({
@@ -32,21 +37,26 @@ const emit = defineEmits(["update:modelValue"]);
 
 // Variables reactivas
 const selectedDate = ref(props.modelValue ? dayjs(props.modelValue).toDate() : new Date());
+//const selectedDate = ref(props.modelValue ? dayjs(props.modelValue).tz("America/Lima").toDate() : new Date());
 
 // Función para desactivar fechas menores a hoy
 const desactivarFechas = (date) => {
   const hoy = dayjs().toDate();
+  //const hoy = dayjs().tz("America/Lima").toDate();
   return date <= hoy;
 };
 
 // Computed para formatear la fecha en dd-MM-yyyy
 const formatearFecha = computed(() => {
   return selectedDate.value ? dayjs(selectedDate.value).format("DD-MM-YYYY") : "";
+  //return selectedDate.value ? dayjs(selectedDate.value).tz("America/Lima").format("DD-MM-YYYY") : "";
 });
 
 // Emitir cambios con formato correcto
 const emitirCambio = (newValue) => {
   selectedDate.value = dayjs(newValue).toDate();
   emit("update:modelValue", dayjs(newValue).format("YYYY-MM-DD"));
+  // selectedDate.value = dayjs(newValue).tz("America/Lima").toDate();
+  // emit("update:modelValue", dayjs(newValue).tz("America/Lima").format("YYYY-MM-DD"));
 };
 </script>
