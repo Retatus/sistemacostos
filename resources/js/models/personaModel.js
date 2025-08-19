fb140125376cb79f0c0c7e7a5b1bca27c98ce93c
rguevarac85 <rguevarac85@hotmail.com>
2025-08-19 17:56:00 +0200

    js base para personas

--- resources/js/models/personaModel.js
+++ resources/js/models/personaModel.js
@@ -1,11 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+
 export default function getPersonaInicial() {
   return {
-    id: 0,
+    id: 1,
     temp_id: uuidv4(),
-    nombre: '',    
-    apellido_paterno: '',
-    apellido_materno: '',
+    nombre: obtenerPasajeroAleatorio().nombres,
+    apellido_paterno: obtenerPasajeroAleatorio().apellido_paterno,
+    apellido_materno: obtenerPasajeroAleatorio().apellido_materno,
     documento_tipo_id: '',
     documento_numero: '',
     pais_id: '',
@@ -18,3 +19,34 @@ export default function getPersonaInicial() {
     estado_activo: 1,
   };
 }
+
+function obtenerPasajeroAleatorio() {
+  const nombres = [
+    "LUIS",
+    "ANA",
+    "CARLOS",
+    "MARÍA",
+    "JOSÉ",
+    "VALERIA",
+    "DIEGO",
+    "LUCÍA",
+    "ANDRÉS",
+    "SOFÍA"
+  ];
+  const apellidos = [
+    "PÉREZ",
+    "GÓMEZ",
+    "HERNÁNDEZ",
+    "LÓPEZ",
+    "MARTÍNEZ",
+    "GARCÍA",
+    "RODRÍGUEZ",
+    "SÁNCHEZ",
+    "RAMÍREZ",
+    "TORO"
+  ];
+
+  const indiceAleatorio = Math.floor(Math.random() * nombres.length);
+  return { nombres: nombres[indiceAleatorio], apellido_paterno: apellidos[indiceAleatorio], apellido_materno: apellidos[indiceAleatorio] };
+}
+
