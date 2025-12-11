eca7b56db552c90ad79c8c8449cf25516395c9b2
rguevarac85 <rguevarac85@hotmail.com>
2025-12-11 18:54:44 +0100

    Correccion de componente para que el Dropdown se cierre al perder el focus

--- resources/js/Components/CustomSelect.vue
+++ resources/js/Components/CustomSelect.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="custom-select relative w-full text-xs">
     <!-- Selector visible -->
-    <div class="selected-option border rounded px-2 py-1 cursor-pointer" @click="toggleDropdown">
-      {{ selectedPasajero?.nombre || 'Pasaejeros asignados' }}
+    <div class="selected-option border rounded px-2 py-2 cursor-pointer select-none border-gray-300 w-full" @click="toggleDropdown" @blur="closeDropdown" tabindex="0">
+      {{ selectedPasajero?.nombre || 'Pasajeros asignados' }}
       <span class="float-right">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-2 h-4 w-4 inline-block ml-2">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-1 h-3 w-3 inline-block ml-2">
           <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
         </svg>
       </span>      
@@ -44,6 +44,11 @@ export default {
     toggleDropdown() {
       this.isOpen = !this.isOpen;
     },
+
+    closeDropdown() {
+      this.isOpen = false;
+    },
+
     selectPasajero(pasajero) {
       this.selectedPasajero = pasajero;
       this.isOpen = false;
